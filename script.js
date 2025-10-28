fbbe954ab5aece5a173a166226c6017b8f041ac9
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-28 11:10:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+ const nxtBtn=document.querySelector('#next');
+    const prevBtn = document.querySelector('#prev')
+
+    const circles = document.querySelectorAll('.circle');
+
+   let circleIndex = 0;
+   const updateActiveCircle=()=>{
+    circles.forEach((c,index)=>{
+        c.classList.toggle('active',index === circleIndex);
+    })
+   }
+nxtBtn.addEventListener('click',()=>{
+    if(circleIndex < circles.length-1){
+        circleIndex++;
+        updateActiveCircle();
+    }
+})
+
+prevBtn.addEventListener('click',()=>{
+    if(circleIndex>0){
+        circleIndex--;
+        updateActiveCircle();
+    }
+})
